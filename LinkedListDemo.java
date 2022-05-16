002003ad435accb5e8c96a74ca4830cdd3bec569
Dhananjay <Dhananjaykhairnar15@gmail.com>
2022-05-16 17:11:13 +0530

    Singly Linked List

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -3,18 +3,20 @@ class Node {
     Node next;
     int data;
 
-    Node(int data) { // ___________
-        next = null; // | | |
-        this.data = data; // --->| data|next |---->null
-    } // |_____|_____|
+    Node(int data) { //                                     ___________
+        next = null; //                                    |     |     |
+        this.data = data; //                           --->| data|next |---->null
+    } //                                                   |_____|_____|
 } // Node
 
 // ---------------------------------------------------------------------------------------------
-// ----------------------------------: Linked list implimentation
-// :-----------------------------
+// ----------------------------------: Linked list implimentation :-----------------------------
 class LinkedList {
     Node head = null;
 
+    //Operations : 
+
+    //1 . Insert or Push Data At the Front of list : O(1)
     void insertFront(int data) {
         Node temp = new Node(data);
         if (head == null) {
@@ -24,6 +26,7 @@ class LinkedList {
         head = temp;
     }
 
+    //2. Insert Data At The End of List : O(n)
     void insertEnd(int data) {
         Node temp = new Node(data);
         temp.next = null;
@@ -34,6 +37,7 @@ class LinkedList {
         curr.next = temp;
     }
 
+    //3. Insert Data After The Given Element : O(n)
     void insertAfter(int data, int element) {
         Node temp = new Node(data);
         temp.next = null;
@@ -45,7 +49,8 @@ class LinkedList {
         curr.next = temp;
     }
 
-    void insertBefore(int data, int element) {
+    //4. Insert Data Before The Given Element : O(n) 
+    void insertBefore(int data, int element) { 
         Node temp = new Node(data);
         temp.next = null;
         Node curr = head;
@@ -61,6 +66,7 @@ class LinkedList {
         curr.next = temp;
     }
 
+    //5. Delete Element From Front of List : O(1)
     int deleteFront() {
         if (head == null) {
             System.out.println("\t* List is Empty : ");
@@ -71,6 +77,7 @@ class LinkedList {
         return x;
     }
 
+    //6. Delete Element From End of List : O(n)
     int deleteEnd() {
         if (head == null) {
             System.out.println("\t* List is Empty : ");
@@ -92,6 +99,7 @@ class LinkedList {
         return x;
     }
 
+    //7. Delete Specific Element from list : O(n)
     int delete(int element) {
         Node temp = head.next, prev = head;
         if (head.data == element) {
@@ -109,6 +117,7 @@ class LinkedList {
         return x;
     }
 
+    //8. Display or Traverse list : O(n)
     void display() {
         if (head == null) {
             System.out.println("\t* List is Empty : ");
@@ -122,12 +131,12 @@ class LinkedList {
             i++;
         }
     }
-
+    //9. Reset List  (Delete all Element from list ) :
     void resetList() {
         head = null;
         System.out.println("\t* List Reset : ");
     }
-
+    //10. Calculate Lenght of List : O(n)
     int listLenght() {
         int count = 0;
         Node temp = head;
@@ -137,7 +146,8 @@ class LinkedList {
         }
         return count;
     }
-
+ 
+    //11 .reverse list permenently : O(n)
     void reverseList() {
         Node prev = null;
         Node curr = head;
@@ -151,6 +161,7 @@ class LinkedList {
         head = prev;
     }
 
+    //12. Sort List : 
     void sortList() {
         Node temp;
         Node tempNode = head;
@@ -182,22 +193,21 @@ public class LinkedListDemo {
         ls.insertBefore(90, 70); // before 70 : Node 0 : or head : 90 70 60 65 50 25 80
         System.out.println("\t* List : ");
         ls.display(); //
+ 
         System.out.println("\t* Sorted list : ");
         ls.sortList();
 
         ls.display();
-        // System.out.println("\t* Lenght of String : "+ls.listLenght());
-        // ls.deleteFront(); // delete 90:
-        // ls.deleteEnd(); // delete 80:
-        // ls.delete(65); // delete 65 ;
-        // System.out.println("\t* head : "+ls.head.data); //head node : 70
-        // System.out.println("\t* List after deletion : ");
-        // ls.display(); // list traversal after deletion :
-        // ls.listLenght();
-        // System.out.println("\t* Lenght of String : "+ls.listLenght());
-        // System.out.println(ls.head.data);
-        // System.out.println("\t* List become permanently reverse : ");
-        // ls.reverseList(); // reverse the list permanently :
-        // ls.display();
+        System.out.println("\t* Lenght of String : "+ls.listLenght());
+        ls.deleteFront(); // delete 90:
+        ls.deleteEnd(); // delete 80:
+        ls.delete(65); // delete 65 ;
+        System.out.println("\t* head : "+ls.head.data); //head node : 70
+        System.out.println("\t* List after deletion : ");
+        ls.display(); // list traversal after deletion :
+        System.out.println("\t* Lenght of String : "+ls.listLenght());
+        System.out.println("\t* List become permanently reverse : ");
+        ls.reverseList(); // reverse the list permanently :
+        ls.display();
     }
 }
